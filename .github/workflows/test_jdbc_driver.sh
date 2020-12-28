2e18632e6fe317a446855199552dc23b49d9fa44
Nguyen Tien Tung Duong <duong1.nguyentientung@toshiba.co.jp>
2020-12-28 17:40:45 +0700

    update

--- .github/workflows/test_jdbc_driver.sh
+++ .github/workflows/test_jdbc_driver.sh
@@ -9,8 +9,12 @@ docker run -d --network="host" --name griddb_sample -e GRIDDB_CLUSTER_NAME=myClu
 # Turn off firewall
 sudo ufw disable
 
+# Get version jdbc driver
+JAVA_VERSION=$(cat pom.xml | grep "version" |  head -n 1 | cut -d ">" -f 2 | cut -d "<" -f 1)
+PACKAGE_NAME=$(cat pom.xml | grep "artifactId" |  head -n 1 | cut -d ">" -f 2 | cut -d "<" -f 1)
+
 # Export PATH and run JDBC sample
-export CLASSPATH=${CLASSPATH}:target/
+export CLASSPATH=${CLASSPATH}:target/$PACKAGE_NAME-$JAVA_VERSION.jar
 cp sample/en/jdbc/JDBCSelect.java .
 javac JDBCSelect.java
 java JDBCSelect
