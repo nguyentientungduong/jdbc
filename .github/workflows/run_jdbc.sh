b2b37840aeb0f73f3b1891ba265f958474303308
Nguyen Tien Tung Duong <49387566+nguyentientungduong@users.noreply.github.com>
2020-12-28 13:53:08 +0700

    Update run_jdbc.sh

--- .github/workflows/run_jdbc.sh
+++ .github/workflows/run_jdbc.sh
@@ -4,7 +4,7 @@
 sudo ufw disable
 
 # Export PATH and run JDBC sample
-export CLASSPATH=${CLASSPATH}:target/
+export CLASSPATH=${CLASSPATH}:bin/gridstore-jdbc.jar
 cp sample/en/jdbc/JDBCSelect.java .
 javac JDBCSelect.java
 java JDBCSelect
