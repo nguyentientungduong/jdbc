e87de6247552cbc4ecfdfd45e7dbeca99a4d9037
Nguyen Tien Tung Duong <49387566+nguyentientungduong@users.noreply.github.com>
2020-12-28 11:09:47 +0700

    Update run_griddb_server.sh

--- .github/workflows/run_griddb_server.sh
+++ .github/workflows/run_griddb_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Create and run griddb server with clustername is "myCluster"
-
-timeout -k 5 10 docker run --network="host" --name griddb_sample -e GRIDDB_CLUSTER_NAME=myCluster griddb/griddb
+docker pull griddb/griddb
+timeout -k 5 15 docker run --network="host" --name griddb_sample -e GRIDDB_CLUSTER_NAME=myCluster griddb/griddb
