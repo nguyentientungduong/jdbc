07e6de32af88620cba31b6a4675ee098883f6677
Nguyen Tien Tung Duong <49387566+nguyentientungduong@users.noreply.github.com>
2020-12-28 10:42:42 +0700

    Update run_jdbc.sh

--- .github/workflows/run_jdbc.sh
+++ .github/workflows/run_jdbc.sh
@@ -4,5 +4,5 @@ docker pull griddb/griddb
 export CLASSPATH=${CLASSPATH}:target/
 cp sample/en/jdbc/JDBCSelect.java .
 javac JDBCSelect.java
-java JDBCSelect
+docker run --network="host" --name griddb_sample -e GRIDDB_CLUSTER_NAME=myCluster griddb/griddb & java JDBCSelect
 docker kill griddb_sample
