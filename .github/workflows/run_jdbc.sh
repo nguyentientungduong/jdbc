ae66a0705eb9c0da312682fbe8af44cddeba8d04
Nguyen Tien Tung Duong <49387566+nguyentientungduong@users.noreply.github.com>
2020-12-28 12:02:56 +0700

    Update run_jdbc.sh

--- .github/workflows/run_jdbc.sh
+++ .github/workflows/run_jdbc.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Turn off firewall
+ufw disable
+
+# Export PATH and run JDBC sample
 export CLASSPATH=${CLASSPATH}:target/
 cp sample/en/jdbc/JDBCSelect.java .
 javac JDBCSelect.java
